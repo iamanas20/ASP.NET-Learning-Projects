120f5b34d66e7711baa9bc166a4d3f1212babaa7
iamanas20 <annouslatique@live.fr>
2018-01-13 13:26:55 +0000

    Add project files.

---
+++ QuickTest/KDNME.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QuickTest
+{
+    public partial class KDNME : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int fontCounter = 9;
+            Color color = Color.Aqua;
+            for (int i = 0; i < 10; i++)
+            {
+                var label = new Label();
+                fontCounter++;
+                label.Font.Size = fontCounter;
+                label.Text = "La page dynamique en ASP.NET, fuck your education you sob!!";
+                label.Font.Size = GetIncrementedLabelFontSize(label);
+                color = GetColor(color);
+                label.ForeColor = color;
+                Form.Controls.Add(label);
+                Form.Controls.Add(new LiteralControl("<br/><br/>"));
+            }
+        }
+
+        private int GetIncrementedLabelFontSize(Label label) => Convert.ToInt16(label.Font.Size.Unit.Value) + 1;
+
+        private Color GetColor(Color color)
+        {
+            var R = IncrementColorComponents(color.R, 70);
+            var G = IncrementColorComponents(color.G, -10);
+            var B = IncrementColorComponents(color.B, -2);
+            return Color.FromArgb(255, R, G, B);
+        }
+
+        private int IncrementColorComponents(int colorComponent, int incrementValue)
+        {
+            if ((colorComponent + incrementValue) < 255)
+            {
+                return colorComponent + incrementValue;
+            }
+            else
+            {
+                return colorComponent;
+            }
+        }
+
+
+    }
+}
---
+++ QuickTest/WebForm2.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="QuickTest.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label Text="Nom" runat="server" />
+            <asp:TextBox ID="NameTextBox" runat="server" />
+            <br />
+            <br />
+            <asp:Button ID="ClickButton" OnClick="ClickButton_Click" Text="Click Me!!!" runat="server" />
+            <br />
+            <br />
+            <asp:Label ID="NameLabel" Text="Bonjour" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
